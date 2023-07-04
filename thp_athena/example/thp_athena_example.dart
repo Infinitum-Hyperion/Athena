cb0f9a5de6755c9e4fa72866befff6b1000f434b
ObsidianXVI <siddharth.chitikela@gmail.com>
2023-07-04 18:50:04 +0800

    Initial commit

---
+++ thp_athena/example/thp_athena_example.dart
@@ -0,0 +1,6 @@
+import 'package:thp_athena/thp_athena.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ thp_athena/test/thp_athena_test.dart
@@ -0,0 +1,16 @@
+import 'package:thp_athena/thp_athena.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
