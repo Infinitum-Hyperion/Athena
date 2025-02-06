8247ce48e20e55e4cc61df7fd026a48ed9acafe0
ObsidianXVI <siddharth.chitikela@gmail.com>
2025-02-06 16:03:42 +0800

    initial commit

---
+++ athena_sdk/bin/athena_sdk.dart
@@ -0,0 +1,5 @@
+import 'package:athena_sdk/athena_sdk.dart' as athena_sdk;
+
+void main(List<String> arguments) {
+  print('Hello world: ${athena_sdk.calculate()}!');
+}
---
+++ athena_sdk/test/athena_sdk_test.dart
@@ -0,0 +1,8 @@
+import 'package:athena_sdk/athena_sdk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
